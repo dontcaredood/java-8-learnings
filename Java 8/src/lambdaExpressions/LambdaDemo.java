16cab06ad5cbdf999201d3588f89c6fafc41a148
santhosh <santhoshkumarpandi0@gmail.com>
2022-03-29 16:43:01 +0530

    Day 1 Learnings..

--- Java 8/src/lambdaExpressions/LambdaDemo.java
+++ Java 8/src/lambdaExpressions/LambdaDemo.java
@@ -1,5 +1,12 @@
 package lambdaExpressions;
 
 public class LambdaDemo {
+    public static void main(String[] args) {
+        Lambda lambda = ()->System.out.println("HEYYA!");
+        lambda.foo();
+    }
+}
 
+interface Lambda{
+    void foo();
 }
